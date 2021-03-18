82bd91e8a65aa8fdb016f7ffe8f544a2309a07ea
TimoWidmann <66517763+TimoWidmann@users.noreply.github.com>
2021-03-18 12:08:44 +0100

    Create README.md

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,7 @@
 
 /** 
  * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ * Timo is testing this
  */
 public class HelloWorld {
   public static void main(String[] args) {
